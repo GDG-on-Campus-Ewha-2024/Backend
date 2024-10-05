75b2318ce31d258b77ea3c2d24e02994945f8bb2
nakyeong <rinarina0429@naver.com>
2024-10-05 22:56:50 +0900

    [FEAT] UserRepository JpaRepository extend 하도록

--- Spring/Week01/hello-spring/src/main/java/helllo/hello_spring/repository/UserRepository.java
+++ Spring/Week01/hello-spring/src/main/java/helllo/hello_spring/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package helllo.hello_spring.repository;
 
+import helllo.hello_spring.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
