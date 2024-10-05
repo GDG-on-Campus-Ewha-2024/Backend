dbebe9cc7873ae223efb14b211b5b5e262073cab
nakyeong <rinarina0429@naver.com>
2024-10-05 22:23:05 +0900

    [FEAT] AutoConfig 클래스 작성

--- Spring/Week01/hello-spring/autoconfigure/src/main/java/org/example/AutoConfig.java
+++ Spring/Week01/hello-spring/autoconfigure/src/main/java/org/example/AutoConfig.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import helllo.hello_spring.controller.HelloController;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AutoConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public HelloController helloController() {
+        return new HelloController();
+    }
 }
