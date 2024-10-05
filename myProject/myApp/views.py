65fffaa88cb4181bab839d8bf73e64ef29bcbe6e
eugene385 <eugene03850@gmail.com>
2024-10-05 13:45:12 +0900

    [10월 5일] 3주차 과제

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Blog
 from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
-
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
-    return render(request, 'index.html')
+    posts_list = Blog.objects.all().order_by('-date')  # 모든 포스트를 가져옵니다.
+    paginator = Paginator(posts_list, 5)  # 페이지당 5개의 포스트
+    page_number = request.GET.get('page')  # 요청된 페이지 번호
+    posts = paginator.get_page(page_number)  # 해당 페이지의 포스트 가져오기
+
+    return render(request, 'index.html', {'posts': posts})
+
+def delete(request, id):
+    if request.method == 'POST':
+        post = get_object_or_404(Blog, id=id)
+        post.delete()
+        return HttpResponse(status=200)  # 성공 시 HTTP 200 응답
+    return HttpResponse(status=405)  # POST 요청이 아닐 경우 405 응답
 
 def new(request):
     return render(request, 'new_post.html')
@@ -16,7 +28,7 @@ def create(request):
     if request.method == 'POST':
         post = Blog()
         post.title = request.POST['title']
-        post.body = request.POST.get('body', '')
+        post.body = request.POST.get('body')
         post.date = timezone.now()
         try:
             post.clean()  # 유효성 검사 호출
@@ -25,12 +37,22 @@ def create(request):
         except ValidationError as e:
             return render(request, 'index.html', {'form': post, 'errors': e.messages})
     
-    return redirect('home')  
+    return redirect('home', post.id)  
 
-def home(request):
-    posts_list = Blog.objects.all().order_by('-date')  # 모든 포스트를 가져옵니다.
-    paginator = Paginator(posts_list, 5)  # 페이지당 5개의 포스트
-    page_number = request.GET.get('page')  # 요청된 페이지 번호
-    posts = paginator.get_page(page_number)  # 해당 페이지의 포스트 가져오기
+def edit_post(request, post_id):
+    post = get_object_or_404(Blog, id=post_id)  # 해당 포스트 가져오기
+    return render(request, 'edit_post.html', {'post': post})
 
-    return render(request, 'index.html', {'posts': posts})
+def update(request, post_id):
+    post = get_object_or_404(Blog, id=post_id)  # 해당 포스트 가져오기
+    if request.method == 'POST':
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.date = timezone.now()
+        try:
+            post.clean()  # 유효성 검사 호출
+            post.save()  # 수정된 데이터 저장
+            return redirect('home')  # 글 목록 페이지로 리디렉션
+        except ValidationError as e:
+            return render(request, 'edit_post.html', {'post': post, 'errors': e.messages})
+    return redirect('home')  # POST가 아닌 경우에는 홈으로 리디렉션
--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -4,8 +4,10 @@ from myApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # Index 페이지
+    path('', views.index, name='home'),  # Index 페이지
     path('new_post/', views.new, name="new_post"),  # 새 글 작성 페이지
     path('create/', views.create, name="create"),  # 글 생성 처리
-    path('home/', views.home, name="home"),  # 홈 페이지
+    path('edit_post/<int:post_id>/', views.edit_post, name="edit_post"),  # 글 수정 페이지
+    path('update/<int:post_id>/', views.update, name="update"),  # 글 수정 처리
+    path('delete/<int:id>/', views.delete, name='delete'),  # 글 삭제 처리
 ]
