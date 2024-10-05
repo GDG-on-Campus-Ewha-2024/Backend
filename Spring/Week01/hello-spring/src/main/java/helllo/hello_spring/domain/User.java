e550b48023baa213329b7b5f6e6f544d31799203
nakyeong <rinarina0429@naver.com>
2024-10-05 22:51:54 +0900

    [FEAT] User 엔티티 추가

---
+++ Spring/Week01/hello-spring/src/main/java/helllo/hello_spring/domain/User.java
@@ -0,0 +1,24 @@
+package helllo.hello_spring.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(columnDefinition = "varchar(10)", nullable = false)
+    private String name;
+}
