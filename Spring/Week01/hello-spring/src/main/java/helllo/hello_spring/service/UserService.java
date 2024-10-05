62f2a03c5cd9319e3c5aa53122b74bea5d26a35a
nakyeong <rinarina0429@naver.com>
2024-10-05 23:09:20 +0900

    [FEAT] 서비스 로직에 스코프 및 콜백 추가

--- Spring/Week01/hello-spring/src/main/java/helllo/hello_spring/service/UserService.java
+++ Spring/Week01/hello-spring/src/main/java/helllo/hello_spring/service/UserService.java
@@ -2,12 +2,16 @@ package helllo.hello_spring.service;
 
 import helllo.hello_spring.domain.User;
 import helllo.hello_spring.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Scope("session")
 public class UserService {
 
     private final UserRepository userRepository;
@@ -16,5 +20,15 @@ public class UserService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new NoSuchElementException("User not found with id: " + userId));
         return user.getName();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("UserService 초기화 - DB 연결");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("UserService 소멸 - DB 연결 해제");
     }
 }
