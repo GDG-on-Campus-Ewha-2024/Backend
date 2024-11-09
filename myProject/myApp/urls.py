2fd4f6a655ed65ff2ca6165cd8ae056bd5d4ce3b
erika0915 <soohee2001@gmail.com>
2024-11-10 00:16:23 +0900

    🎨 폴더 구조와 이름 변경

--- myProject/myApp/urls.py
+++
@@ -1,9 +0,0 @@
-# myApp/urls.py 
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('posts/', views.post_list, name='post_list'), # 게시물 리스트 조회 
-    path('posts/new', views.new, name = 'post_new'), # 새 게시물 작성 폼 
-    path('posts/create/', views.post_create, name='post_create'), # 새 게시물 생성 
-]
--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myApp',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,5 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'myApp.UserModel'
