ecf0b5974a3aa3e4a0fa19f8da6d1a89aadd3551
erika0915 <soohee2001@gmail.com>
2024-11-10 02:23:03 +0900

    ✨ WIL-2주차 수정

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 메인 페이지에 대한 요청은 myApp의 urls.py로 전달됨 
-    # path('', include('myApp.urls')), 
-
+    #path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
 ]
