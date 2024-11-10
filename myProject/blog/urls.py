d2b42d074fe70d2d7d6b23deef87c3f4c7bcde54
erika0915 <soohee2001@gmail.com>
2024-11-10 13:04:34 +0900

    ✨ WIL-3주차 코드 수정 완료

--- myProject/blog/urls.py
+++ myProject/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),     # read : 게시글 목록 
+    path('<int:post_id>/', views.post_detail, name='post_detail'),  # read : 게시글 상세 
+    path('create/', views.post_create, name='post_create'), # create: 새 게시글 작성 
+    path('<int:post_id>/edit/', views.post_edit, name='post_edit'),  # update : 게시글 수정 
+    path('<int:post_id>/delete/', views.post_delete, name='post_delete'), # delete : 게시글 삭제
 ]
--- myProject/blog/views.py
+++ myProject/blog/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
 
+# 게시글 목록 조회 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts' : posts})
 
+# 게시글 상세 조회 
 def post_detail(request, post_id):
-    return render(request, 'blog/post_detail.html', {'post_id' : post_id})
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post' : post})
+
+# 새 게시글 작성 
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_list')
+    else: 
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form' : form})
+    
+# 게시글 수정 
+def post_edit(request, post_id): 
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id = post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form':form})
+
+# 게시글 삭제 
+def post_delete(request,  post_id): 
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete() 
+        return redirect('post_list')
+    
+    return render(request, 'blog/post_confirm_delete.html', {'post' : post})
