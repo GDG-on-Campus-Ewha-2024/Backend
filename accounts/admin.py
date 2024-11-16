eb8b1d8b200ac162154648db44e919ba2c66e60c
erika0915 <soohee2001@gmail.com>
2024-11-16 23:08:08 +0900

    chore: account 앱 삭제

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'nickname', 'email', 'phone_number', 'birthday', 'address', 'is_staff', 'is_active']
-    search_fields = ['username', 'email', 'nickname']
-    list_filter = ['is_staff', 'is_active', 'birthday']
-    ordering = ['username']
--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    # path('', include('allauth.urls')),
-    path('logout/', views.logout_view, name='logout'),
-]
