5d5acc676b381bfc2d0730ad2b0e5739eb4bbc59
erika0915 <soohee2001@gmail.com>
2024-11-16 23:46:42 +0900

    chore: 네이버 로그인까지 다시 수정

--- myProject/settings.py
+++ myProject/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.naver',
 
-    'accounts',
     'blog',
     'core',
 ]
@@ -150,12 +149,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
 
